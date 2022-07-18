df4ca3bd6aaad5a4161635c805911a11662e873f
jadelafuente <40989492+jadelafuente@users.noreply.github.com>
2022-07-18 17:45:13 +0200

    Update GreetingResourceNewCallTest.java
    Update file for testing new call

--- src/test/java/org/acme/GreetingResourceNewCallTest.java
+++ src/test/java/org/acme/GreetingResourceNewCallTest.java
@@ -1,1 +1,22 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GreetingResourceNewCallTest {
+
+    @Test
+    public void testHelloNewCallEndpoint() {
+        given()
+          .when().get("/hello-newcall")
+          .then()
+             .statusCode(200)
+             .body(is("Hello NewCall Quarkus Java Test Answer: Pilot for new answers"));
+    }
+
+}
 
